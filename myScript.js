f252d69d010ef3dbab5d29de83f49158e2fe6c0b
Cesar Gomez <zootechdrum@gmail.com>
2020-03-25 16:56:48 -0700

    successfully looped through date/ rows show up on html page

--- myScript.js
+++ myScript.js
@@ -1,6 +1,55 @@
 $( document ).ready(()=> {
-    console.log(moment().format("h"))
+
     //appends the current date to jumbotron
     $("#currentDay").append(moment().format("L"))
+
+    
+
+    for(let i = 9; i <= 18; i++){
+      let row = $("<div class='row time-block'>")
+        const col1 = $("<div class='col-sm-2 hour'><p class='time-block'>"+i+"</p></div>");
+        const col2 = $("<div class='col-sm-8 present'><textarea class='description' name='' id='' cols='80' rows='1'></textarea></div>")
+        const col3 = $("<div class='col-sm-2 saveBtn'><button class='saveBtns'>k</button>")
+        row.append(col1)
+        row.append(col2)
+        row.append(col3)
+        $(".container").append(row)
+
+    }
+
+
+
 })
 
+{/* <div class="container">
+<div class="row time-block">
+  <div class="col-sm-2 hour">
+      <p class="time-block">
+        8:00
+      </p>
+    </div>
+    <div class="present col-sm-8">
+      <textarea class="description" name="" id="" cols="80" rows="1"></textarea>
+    </div>
+    <div class="col-sm-2 saveBtn">
+        <button class="saveBtns">
+H
+        </button>
+    </div>
+  </div>
+  <div class="row time-block">
+      <div class="col-sm-2  hour">
+          <p class="time-block">
+            8:00
+          </p>
+        </div>
+        <div class="present col-sm-8">
+          <textarea class="description" name="" id="" cols="80" rows="1"></textarea>
+        </div>
+        <div class="col-sm-2 saveBtn">
+            <button class="saveBtns">
+H
+            </button>
+        </div>
+      </div>
+</div> */}

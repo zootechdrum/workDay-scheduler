bb39f73460d76762224008f079f21e130d27e9e5
Cesar Gomez <zootechdrum@gmail.com>
2020-03-25 17:03:47 -0700

    successfuly restarted hours after reaching 12 oclock

--- myScript.js
+++ myScript.js
@@ -3,18 +3,23 @@ $( document ).ready(()=> {
     //appends the current date to jumbotron
     $("#currentDay").append(moment().format("L"))
 
-    
+    let hours = 9;
 
-    for(let i = 9; i <= 18; i++){
-      let row = $("<div class='row time-block'>")
-        const col1 = $("<div class='col-sm-2 hour'><p class='time-block'>"+i+"</p></div>");
+    for(let i = 9; i < 18; i++){
+
+      if(hours === 13){
+        hours = 1;
+      }
+
+        const row = $("<div class='row time-block'>")
+        const col1 = $("<div class='col-sm-2 hour'><p class='time-block'>"+hours+"</p></div>");
         const col2 = $("<div class='col-sm-8 present'><textarea class='description' name='' id='' cols='80' rows='1'></textarea></div>")
         const col3 = $("<div class='col-sm-2 saveBtn'><button class='saveBtns'>k</button>")
         row.append(col1)
         row.append(col2)
         row.append(col3)
         $(".container").append(row)
-
+        ++hours
     }
 
 

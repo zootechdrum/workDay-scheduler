01d7976af295df8cf5c0e231911708399496b51f
Cesar Gomez <zootechdrum@gmail.com>
2020-03-26 19:05:30 -0700

    changed font for font awesome icon also setInterval to update colors every hours

--- myScript.js
+++ myScript.js
@@ -1,43 +1,54 @@
-$( document ).ready(()=> {
+$(document).ready(() => {
 
-    //appends the current date to jumbotron
-    $("#currentDay").append(moment().format("L"))
+  //appends the current date to jumbotron
+  $("#currentDay").append(moment().format("L"))
 
 
-    for(let i = 9; i < 18; i++){
-        const row = $("<div class='row time-block'>")
-        const col1 = $("<div class='col-sm-2 hour'><p class='time-block'>"+formatAMPM(i)+"</p></div>");
-        const col2 = $("<div id="+(i)+" data-time="+ i +" class='col-sm-8'><textarea class='description' name='' cols='80' rows='1'></textarea></div>")
-        const col3 = $("<div class='col-sm-2 saveBtn d-flex align-items-center justify-content-center'><i class='fas fa-save'></i></div>")
-        row.append(col1)
-        row.append(col2)
-        row.append(col3)
-        $(".container").append(row)
-    }
+  for (let i = 9; i < 18; i++) {
+    const row = $("<div class='row time-block'>")
+    const col1 = $("<div class='col-sm-2 hour'><p class='time-block'>" + formatAMPM(i) + "</p></div>");
+    const col2 = $("<div id=" + (i) + " data-time=" + i + " class='col-sm-8'><textarea class='description' name='' cols='80' rows='1'></textarea></div>")
+    const col3 = $("<div class='col-sm-2 saveBtn d-flex align-items-center justify-content-center'><i class='fas fa-save'></i></div>")
+    row.append(col1)
+    row.append(col2)
+    row.append(col3)
+    $(".container").append(row)
+  }
 
-    function formatAMPM(hours) {
-      var ampm = hours >= 12 ? 'pm' : 'am';
-      hours = hours % 12;
-      hours = hours ? hours : 12;
-      return hours + ampm;
-    }
+  function formatAMPM(hours) {
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    return hours + ampm;
+  }
 
   //change colors of text area depending on the current time
 
-let currentHour = moment().hour()
+  const updateColor = () => {
+    console.log("H")
+    let currentHour = moment().hour()
 
 
-    for(let j=9; j < 18; j++ ){
-      if(currentHour > $("#"+j).data("time")){
-        $("#"+j).addClass("past")
+    for (let j = 9; j < 18; j++) {
+      if (currentHour > $("#" + j).data("time")) {
+        $("#" + j).addClass("past")
 
-      }else if(currentHour === $("#"+j).data("time")){
-      $("#"+j).addClass("present")
-    } else if( currentHour <  $("#"+j).data("time")){
-      $("#"+j).addClass("future")
+      } else if (currentHour === $("#" + j).data("time")) {
+        $("#" + j).addClass("present")
+      } else if (currentHour < $("#" + j).data("time")) {
+        $("#" + j).addClass("future")
+      }
     }
+
   }
+
+  const callEveryHour = () => {
+    setInterval(updateColor(), 100 * 60 * 60)
+  }
+
+  callEveryHour()
 })
+
 
 
 

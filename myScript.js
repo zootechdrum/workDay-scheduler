a99e2ea516e3c0c6eab0142a985a31981a4fa58c
Cesar Gomez <zootechdrum@gmail.com>
2020-03-29 12:41:31 -0700

    added work schedule and can now retrieve items from local storage

--- myScript.js
+++ myScript.js
@@ -9,7 +9,7 @@ $(document).ready(() => {
   for (let i = 9; i < 18; i++) {
     const row = $("<div class='row time-block'>")
     const col1 = $("<div class='col-sm-2 hour'><p class='time-block'>" + formatAMPM(i) + "</p></div>");
-    const col2 = $("<div id=" + (i) + " data-time=" + i + " class='col-sm-8'><textarea id=text-"+(i)+" class='description' name='' cols='80' rows='1'></textarea></div>")
+    const col2 = $("<div id=" + (i) + " data-time=" + i + " class='col-sm-8'><textarea id=text-" +i+ " class='description' name='' cols='80' rows='1'></textarea></div>")
     const col3 = $("<div  class='col-sm-2 saveBtn d-flex align-items-center justify-content-center' data-button="+i+"><i class='fas fa-save'></i></div>")
     row.append(col1)
     row.append(col2)
@@ -48,20 +48,39 @@ $(document).ready(() => {
 
 
 //When save button is pressed
-$("#container").on("click",'.saveBtn', () => {
+$("#container").on("click",'.saveBtn', function() {
 
   const dataIdButton = $(this).attr("data-button");
   const text = $("#text-"+dataIdButton).val()
   
+  if(text.length !== 0){
+  //save in local storage 
+  localStorage.setItem(dataIdButton, text)
+
+  }
 })
 
 const checkLocalStoreage = () => {
+
   const currentDate = moment().format("L")
+
   if(!localStorage.getItem("date")){
     localStorage.setItem("date" , currentDate )
   }else if(localStorage.getItem("date") !== currentDate){
     localStorage.clear()
+    localStorage.setItem("date" , currentDate )
+  }
 
+  if(localStorage.length > 1 && localStorage.getItem("date") === currentDate){
+    for(let i = 0; i < localStorage.length; i++){
+      console.log($("#text-9"))
+      
+      if(localStorage.key(i) !== "date"){
+        console.log($("#text-"+localStorage.key(i)))
+        $("#text-"+localStorage.key(i)).val(localStorage.getItem(localStorage.key(i))) 
+      }
+
+    }
   }
   
 
